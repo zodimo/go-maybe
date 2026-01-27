a21aa6d8ec5d35c785b2b256751a582f6d2f3000
Jaco Labuschagne <zodimo@gmail.com>
2026-01-27 09:58:23 +0200

    move match to helpers

--- helpers.go
+++ helpers.go
@@ -12,3 +12,10 @@ func FlatMap[T any, R any](m Maybe[T], f func(T) Maybe[R]) Maybe[R] {
 	}
 	return None[R]()
 }
+
+func Match[T, U any](m Maybe[T], onSome func(T) U, onNone func() U) U {
+	if m.hasValue {
+		return onSome(m.value)
+	}
+	return onNone()
+}
